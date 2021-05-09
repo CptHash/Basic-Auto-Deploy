2d90f3eb01dfe677a5fe71f734597e914881f8d0
Mathieu HOULLE <mathieu.houlle@epitech.eu>
2021-05-09 11:20:10 +0200

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function log {
+  echo $(date) "--> " $1 >> ~/deploy.log
+  sudo cp ~/deploy.log /var/www/html/deploy.log
+  curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"--> $1\"}" WEBHOOK_URL_HERE
+
+cd ~/codereview.fr
+
+log "Pulling repository..."
+git pull
+
+log "Running npm install..."
+npm install
+
+log "Building project..."
+npm run build
+
+log "Moving build in www folder..."
+sudo rm -rf /var/www/html
+sudo mkdir /var/www/html
+sudo mv build/* /var/www/html
+
+log "Application ready!"
